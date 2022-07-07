2074704f0ffddda11fde0038f52eaf8ae4f6bec6
Petreti Andrea <petretiandrea@gmail.com>
2022-07-07 18:41:52 +0200

    fix bash

--- .github/actions/python-on-arch/run-python-on.sh
+++ .github/actions/python-on-arch/run-python-on.sh
@@ -4,16 +4,16 @@ PYTHON_VERSION=$1
 ARCHITECTURE=$2
 
 # TODO: add ARCHITECTURE check as second arguments ARCHITECTURE=$2
-if [ "$PYTHON_VERSION" = "py3.9" && "$ARCHITECTURE" = "armv7" ]; then
+if [ "$PYTHON_VERSION" = "py3.9" ] && [ "$ARCHITECTURE" = "armv7" ]; then
     IMAGE_NAME=arm32v7/python:3.9
     DOCKER_PLATFORM=linux/arm
-elif [ "$PYTHON_VERSION" = "py3.9" && "$ARCHITECTURE" = "arm64" ]; then
+elif [ "$PYTHON_VERSION" = "py3.9" ] && [ "$ARCHITECTURE" = "arm64" ]; then
     IMAGE_NAME=arm64v8/python:3.9
     DOCKER_PLATFORM=linux/arm64
-elif [ "$PYTHON_VERSION" = "py3.10" && "$ARCHITECTURE" = "armv7" ]; then
+elif [ "$PYTHON_VERSION" = "py3.10" ] && [ "$ARCHITECTURE" = "armv7" ]; then
     IMAGE_NAME=arm32v7/python:3.10
     DOCKER_PLATFORM=linux/arm
-elif [ "$PYTHON_VERSION" = "py3.10" && "$ARCHITECTURE" = "arm64" ]; then
+elif [ "$PYTHON_VERSION" = "py3.10" ] && [ "$ARCHITECTURE" = "arm64" ]; then
     IMAGE_NAME=arm64v8/python:3.10
     DOCKER_PLATFORM=linux/arm64
 fi
