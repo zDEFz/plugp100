a7d64bd213a56d01dd574b814d61737ba8a1002c
Petreti Andrea <petretiandrea@gmail.com>
2022-07-08 21:43:03 +0200

    fix rename wheels script

--- build_tools/rename_wheels.py
+++ build_tools/rename_wheels.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     # replace platform (e.g. linux --> manylinux)
     generated_wheel_names = list(filter(lambda e: '.whl' in e, os.listdir('dist/')))
     for generated_wheel_name in generated_wheel_names:
-        new_wheel_name = re.sub(wheel_regex, rf'\1\2\3\4{platform_name}\6.whl', generated_wheel_name)
+        new_wheel_name = re.sub(wheel_regex, rf'\1\2\3\4{platform_name}.whl', generated_wheel_name)
         os.rename(os.path.join('dist/', generated_wheel_name), os.path.join('dist/', new_wheel_name))
