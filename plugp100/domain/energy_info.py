b2c8670b9d3cfbdec1f945d450dcc82b95085353
Andrea <petretiandrea@gmail.com>
2022-10-21 23:37:30 +0200

    hotfix past1y energy field

--- plugp100/domain/energy_info.py
+++ plugp100/domain/energy_info.py
@@ -8,7 +8,6 @@ class EnergyInfo:
     month_runtime: float = property(lambda self: self.info["month_runtime"])
     today_energy: float = property(lambda self: self.info["today_energy"])
     month_energy: float = property(lambda self: self.info["month_energy"])
-    this_month_energy: float = property(lambda self: self.info['past1y'][11])
     current_power: float = property(lambda self: self.info["current_power"])
 
     def __init__(self, info: Dict[str, Any]):
