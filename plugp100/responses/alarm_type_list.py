ad5d27d072d6fa9bf2a9a2ebb51ea97fc3f719a8
petretiandrea <petretiandrea@gmail.com>
2023-07-20 08:26:16 +0200

    fix try_from_json alarm tones

--- plugp100/responses/alarm_type_list.py
+++ plugp100/responses/alarm_type_list.py
@@ -1,13 +1,20 @@
 from dataclasses import dataclass
 from typing import List, Any
 
+from plugp100.common.functional.either import Left, Right, Either
+
 
 @dataclass
 class AlarmTypeList(object):
     tones: List[str]
 
     @staticmethod
-    def try_from_json(kwargs: dict[str, Any]):
-        return AlarmTypeList(
-            kwargs.get('alarm_type_list', []),
-        )
+    def try_from_json(kwargs: dict[str, Any]) -> Either['AlarmTypeList', Exception]:
+        try:
+            return Right(
+                AlarmTypeList(
+                    kwargs.get('alarm_type_list', []),
+                )
+            )
+        except Exception as e:
+            return Left(e)
