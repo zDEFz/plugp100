2e882a0ed80f5e060a84091514ecd9037d34a8c3
petretiandrea <petretiandrea@gmail.com>
2023-12-20 22:15:27 +0100

    working on camera hub protocol

--- plugp100/example.py
+++ plugp100/example.py
@@ -1,29 +1,41 @@
 import asyncio
 import os
 
-from plugp100.api.light_effect_preset import LightEffectPreset
+import aiohttp
+
 from plugp100.api.tapo_client import TapoClient
 from plugp100.common.credentials import AuthCredential
-from plugp100.discovery.tapo_discovery import TapoDeviceFinder
+from plugp100.protocol.camera_like_protocol import CameraLikeProtocol
+from plugp100.requests.tapo_request import TapoRequest
 
 
 async def main():
-    print("Scanning network...")
-    print(TapoDeviceFinder.classify(TapoDeviceFinder.scan(5)))
+    # print("Scanning network...")
+    # print(TapoDeviceFinder.classify(TapoDeviceFinder.scan(5)))
 
     # create generic tapo api
     username = os.getenv("USERNAME", "<tapo_email>")
     password = os.getenv("PASSWORD", "<tapo_password>")
+    credentials = AuthCredential("admin", "")
+    # client = TapoClient.create(credentials, "192.168.1.10")
+    # print(await client.execute_raw_request(TapoRequest(method="get_sysinfo", params=None)))
 
-    credentials = AuthCredential(username, password)
-    client = TapoClient.create(credentials, "<tapo_device_ip>")
+    protocol = CameraLikeProtocol(
+        credentials, "4.tcp.eu.ngrok.io", 18938, aiohttp.ClientSession()
+    )
 
-    print(await client.get_device_info())
-    print(await client.get_energy_usage())
-    print(await client.get_current_power())
-    print(await client.get_child_device_list())
-    print(await client.get_child_device_component_list())
-    print(await client.set_lighting_effect(LightEffectPreset.Aurora.to_effect()))
+    print(await protocol.send_request(TapoRequest.get_device_info()))
+
+    #
+    # components = await client.get_component_negotiation()
+    # print(components)
+
+    # print(await client.get_device_info())
+    # print(await client.get_energy_usage())
+    # print(await client.get_current_power())
+    # print(await client.get_child_device_list())
+    # print(await client.get_child_device_component_list())
+    # print(await client.set_lighting_effect(LightEffectPreset.Aurora.to_effect()))
     # plug = PlugDevice(TapoClient(username, password), "<tapo_device_ip>")
     # light = LightDevice(TapoClient(username, password), "<tapo_device_ip>")
     # ledstrip = LedStripDevice(TapoClient(username, password), "<tapo_device_ip>")
